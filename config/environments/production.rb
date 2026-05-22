99383e9f4cc955d30e919fb2ae0b0c3099d01b91
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2026-05-21 23:26:44 -0300

    ajusta a imagem no coffess/show.html.erb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
+  # Avoid browser preload warnings for assets delivered via Link headers.
+  config.action_view.preload_links_header = false
+
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
